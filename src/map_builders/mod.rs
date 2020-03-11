5111d1237b25ccfa58f33c3a6a2c7a1137e02210
Ysgard <ysgard@gmail.com>
2020-03-11 09:22:10 -0700

    3.7 Maze/Labyrinth Generation

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -5,11 +5,13 @@ mod bsp_dungeon;
 mod bsp_interior;
 mod cellular_automata;
 mod drunkard;
+mod maze;
 mod simple_map;
 use bsp_dungeon::BspDungeonBuilder;
 use bsp_interior::BspInteriorBuilder;
 use cellular_automata::CellularAutomataBuilder;
 use drunkard::DrunkardsWalkBuilder;
+use maze::MazeBuilder;
 use simple_map::SimpleMapBuilder;
 
 use specs::prelude::*;
@@ -25,7 +27,7 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 7);
+    let builder = rng.roll_dice(1, 8);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
@@ -33,6 +35,7 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
         4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
         5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
         6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+        7 => Box::new(MazeBuilder::new(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
